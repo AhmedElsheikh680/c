9b7eba16f993346801f4a56f23a6d8b0a9ae44f0
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-11-26 16:50:08 -0800

    power(base, exponential)

---
+++ lab5/power/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+int power(int base, int exponential);
+int main()
+{
+    int base, power;
+    printf("Enter Base Number: ");
+    scanf("%d", &base);
+    printf("Enter Power Number: ");
+    scanf("%d", &power);
+    printf("%d ^ %d = %d", base, power, calcPower(base, power));
+    return 0;
+}
+
+int calcPower(int base, int exponential) {
+    int result=1;
+    for(int i=1; i<=exponential; i++) {
+        result *=base;
+    }
+    return result;
+}
